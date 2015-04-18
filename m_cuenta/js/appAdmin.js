71b37e49eb015b5b5770a9e389f6e3284cf8376a
Armando Enrique Pisfil Puemape <armandoaepp@gmail.com>
2015-04-18 11:07:26 -0500

    platilla ok}

--- m_cuenta/js/appAdmin.js
+++ m_cuenta/js/appAdmin.js
@@ -2,11 +2,24 @@ angular.module('appAdmin', [
 	'ui.router',
     'ui.bootstrap',
 ])
-    .config([$stateProvider, $urlRouterProvider ,
-        function($stateProvider, $urlRouterProvider){
+.run(
+  [          '$rootScope', '$state', '$stateParams',
+    function ($rootScope,   $state,   $stateParams) {
+
+    // It's very handy to add references to $state and $stateParams to the $rootScope
+    // so that you can access them from any scope within your applications.For example,
+    // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
+    // to active whenever 'contacts.list' or one of its decendents is active.
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+    }
+  ]
+)
+.config(['$stateProvider', '$urlRouterProvider',
+    function ($stateProvider,   $urlRouterProvider) {
             // Use $urlRouterProvider to configure any redirects (when) and invalid urls (otherwise).
             $urlRouterProvider.
-                otherwise('/home');
+                otherwise('/');
 
             $stateProvider
                 .state('home',{
