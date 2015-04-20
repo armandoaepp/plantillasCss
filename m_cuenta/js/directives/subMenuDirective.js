d37ca9904e9bf275e0e5ba013d24c63df22b12cb
Armando Enrique Pisfil Puemape <armandoaepp@gmail.com>
2015-04-20 09:48:00 -0500

    add

--- m_cuenta/js/directives/subMenuDirective.js
+++ m_cuenta/js/directives/subMenuDirective.js
@@ -16,26 +16,8 @@ myApp.directive('subMenu', function(){
 				console.log(hermanos) ;
 
 				 var sub_menu = hermanos[1] ;
-				console.log(sub_menu) ;
-
-
 
 				 angular.element(sub_menu).removeClass('collapse');
-
-
-				// quitar marco
-				/*var padre = element.parent();
-				var hermanos = padre.children();
-
-				for (var i = 0; i < hermanos.length; i++) {
-					var cssOld = hermanos[i].className;
-					var cssNew = '';
-
-					if (cssOld.search(css) > -1) {
-						cssNew = cssOld.replace(css, '');
-						hermanos[i].className = cssNew;
-					}
-				};*/
 
 			});
 		}
